9a51221b2a8e6301545d8d2470435839ae3276e2
adminis <adminis@K037.chdk.lc>
2020-03-22 14:27:08 +0200

    Updates

---
+++ precache-manifest.a0c3be7b8e4431332db10444e89aa9f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5734d4cdc776c19692c1177943a46abb",
+    "url": "/Rock-paper-scissors.github.io/index.html"
+  },
+  {
+    "revision": "c2f1502f30204f8476d7",
+    "url": "/Rock-paper-scissors.github.io/static/js/2.1ac61b24.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Rock-paper-scissors.github.io/static/js/2.1ac61b24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d0ca543e015bf0cb002",
+    "url": "/Rock-paper-scissors.github.io/static/js/main.b98d4d1f.chunk.js"
+  },
+  {
+    "revision": "bd79e6e5e93c006d27ba",
+    "url": "/Rock-paper-scissors.github.io/static/js/runtime-main.20ae327d.js"
+  }
+]);
